0bb1bac3a945422115238ad1ba53453bf8ddfc39
adikus <andrej.hoos@gmail.com>
2016-02-02 16:59:02 +0000

    add license stuff

--- cl-helper.c
+++ cl-helper.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 Andreas Kloeckner
+ * Copyright (c) 2010 Andreas Kloeckner, 2016 Andrej Hoos
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
